42cd8b5232d6411902062f7bc35325883880096b
Martin Angers <martin.n.angers@gmail.com>
2016-03-21 12:39:34 -0400

    juggler/internal/redistest: add a StartCluster func to test with a redis cluster

--- internal/redistest/server.go
+++ internal/redistest/server.go
@@ -2,6 +2,7 @@
 package redistest
 
 import (
+	"fmt"
 	"io"
 	"net"
 	"os/exec"
@@ -15,11 +16,11 @@ import (
 
 // ClusterConfig is the configuration to use for servers started in
 // redis-cluster mode. The value must contain a single reference to
-// an integer placeholder (%d), the port number.
+// a string placeholder (%s), the port number.
 var ClusterConfig = `
-port %d
+port %s
 cluster-enabled yes
-cluster-config-file nodes.%[1]d.conf
+cluster-config-file nodes.%[1]s.conf
 cluster-node-timeout 5000
 appendonly yes
 `
@@ -33,16 +34,50 @@ appendonly yes
 // written to it. If a configuration is specified, it is supplied
 // to the server via stdin.
 func StartServer(t *testing.T, w io.Writer, conf string) (*exec.Cmd, string) {
-	return startServerWithConfig(t, w, conf)
-}
-
-func startServerWithConfig(t *testing.T, w io.Writer, conf string) (*exec.Cmd, string) {
 	if _, err := exec.LookPath("redis-server"); err != nil {
 		t.Skip("redis-server not found in $PATH")
 	}
 
 	port := getFreePort(t)
-	c := exec.Command("redis-server", "--port", port)
+	return startServerWithConfig(t, port, w, conf), port
+}
+
+// StartCluster starts a redis cluster of 3 nodes using the
+// ClusterConfig variable as configuration. If w is not nil,
+// stdout and stderr of each node will be written to it.
+//
+// It returns a function that should be called after the test
+// (typically in a defer), and the list of ports for all nodes
+// in the cluster.
+func StartCluster(t *testing.T, w io.Writer) (func(), []string) {
+	if _, err := exec.LookPath("redis-server"); err != nil {
+		t.Skip("redis-server not found in $PATH")
+	}
+
+	const numNodes = 3
+
+	cmds := make([]*exec.Cmd, numNodes)
+	ports := make([]string, numNodes)
+	for i := 0; i < numNodes; i++ {
+		port := getFreePort(t)
+		cmd := startServerWithConfig(t, port, w, fmt.Sprintf(ClusterConfig, port))
+		cmds[i], ports[i] = cmd, port
+	}
+
+	return func() {
+		for _, c := range cmds {
+			c.Process.Kill()
+		}
+	}, ports
+}
+
+func startServerWithConfig(t *testing.T, port string, w io.Writer, conf string) *exec.Cmd {
+	var args []string
+	if conf == "" {
+		args = []string{"--port", port}
+	}
+	c := exec.Command("redis-server", args...)
+
 	if w != nil {
 		c.Stderr = w
 		c.Stdout = w
@@ -50,6 +85,7 @@ func startServerWithConfig(t *testing.T, w io.Writer, conf string) (*exec.Cmd, s
 	if conf != "" {
 		c.Stdin = strings.NewReader(conf)
 	}
+
 	require.NoError(t, c.Start(), "start redis-server")
 
 	// wait for the server to start accepting connections
@@ -67,7 +103,7 @@ func startServerWithConfig(t *testing.T, w io.Writer, conf string) (*exec.Cmd, s
 	require.True(t, ok, "wait for redis-server to start")
 
 	t.Logf("redis-server started on port %s", port)
-	return c, port
+	return c
 }
 
 func getFreePort(t *testing.T) string {
