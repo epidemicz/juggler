e22bce06568d79988ab15a35cdd491028ecc973f
Martin Angers <martin.n.angers@gmail.com>
2016-03-23 13:36:55 -0400

    juggler/internal/redistest: implement a mock redis server

--- internal/redistest/mock_server.go
+++ internal/redistest/mock_server.go
@@ -1,10 +1,13 @@
 package redistest
 
 import (
-	"io"
+	"bufio"
 	"net"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/PuerkitoBio/gred/resp"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,17 +15,87 @@ import (
 type MockServer struct {
 	Addr string
 
-	l net.Listener
+	done chan struct{}
+	wg   sync.WaitGroup
+	h    func(string, ...string) interface{}
+	t    *testing.T
+	l    net.Listener
 }
 
-// StartMockServer creates and starts a mock redis server.
-func StartMockServer(t *testing.T, handler func(w io.Writer, cmd string, args ...interface{})) *MockServer {
+// StartMockServer creates and starts a mock redis server. The handler is
+// called for each command received by the server. The returned value is
+// encoded in the redis protocol and sent to the client. The caller should close
+// the server after use.
+func StartMockServer(t *testing.T, handler func(cmd string, args ...string) interface{}) *MockServer {
 	l, err := net.Listen("tcp", ":0")
 	require.NoError(t, err, "net.Listen")
 
-	ms := &MockServer{
+	s := &MockServer{
 		Addr: l.Addr().String(),
+		done: make(chan struct{}),
+		h:    handler,
+		t:    t,
 		l:    l,
 	}
-	return ms
+	go s.serve()
+	return s
+}
+
+// Close closes the mock redis server.
+func (s *MockServer) Close() {
+	select {
+	case <-s.done:
+		return
+	default:
+	}
+
+	require.NoError(s.t, s.l.Close(), "Close listener")
+	<-s.done
+	exit := make(chan struct{})
+	go func() {
+		s.wg.Wait()
+		close(exit)
+	}()
+
+	// wait for a few seconds for connections to finish, otherwise fail
+	select {
+	case <-exit:
+		return
+	case <-time.After(5 * time.Second):
+		s.t.Fatal("failed to cleanly stop the mock server")
+	}
+}
+
+func (s *MockServer) serve() {
+	defer close(s.done)
+	for {
+		conn, err := s.l.Accept()
+		if err != nil {
+			return
+		}
+		s.wg.Add(1)
+		go s.serveConn(conn)
+	}
+}
+
+func (s *MockServer) serveConn(c net.Conn) {
+	defer s.wg.Done()
+
+	go func() {
+		<-s.done
+		c.Close()
+	}()
+
+	br := bufio.NewReader(c)
+	for {
+		// Get the request
+		ar, err := resp.DecodeRequest(br)
+		if err != nil {
+			return
+		}
+
+		// Handle the response
+		v := s.h(ar[0], ar[1:]...)
+		require.NoError(s.t, resp.Encode(c, v), "Encode")
+	}
 }
