d2612b058b741825f8ff2a8ad9a1d65f7c5af292
Martin Angers <martin.n.angers@gmail.com>
2016-03-28 12:02:31 -0400

    juggler/misc/perf: more progress on automated load test script

--- misc/perf/run-do-test.sh
+++ misc/perf/run-do-test.sh
@@ -35,32 +35,36 @@ then
         --ssh-keys ${JUGGLER_DO_SSHKEY} \
         --wait
 
-    # get the redis IP address
+    # start redis on the expected port and with the right config
     dropletname=juggler-redis
     getip='doctl compute droplet list --format PublicIPv4 --no-header ${dropletname} | head -n 1'
     redisip=$(eval ${getip})
     echo "redis IP: " ${redisip}
-
-    # start redis on the expected port and with the right config
-    ssh -n -f root@${redisip} "sh -c 'pkill redis-server; echo 511 > /proc/sys/net/core/somaxconn; nohup redis-server --port 9000 --maxclients 100000 > /dev/null 2>&1 &'"
+    ssh-keygen -R ${redisip}
+    ssh -n -f -oStrictHostKeyChecking=no root@${redisip} "sh -c 'pkill redis-server; echo 511 > /proc/sys/net/core/somaxconn; nohup redis-server --port 7000 --maxclients 100000 > /dev/null 2>&1 &'"
 
     # copy the server to juggler-server
     dropletname=juggler-server
     serverip=$(eval ${getip})
     echo "server IP: " ${serverip}
-    scp -C juggler-server root@${serverip}:~
+    ssh-keygen -R ${serverip}
+    scp -C -oStrictHostKeyChecking=no juggler-server root@${serverip}:~
+    ssh -n -f root@${serverip} "sh -c 'nohup ~/juggler-server -L -redis=${redisip}:7000'"
 
     # copy the callee to juggler-callee
     dropletname=juggler-callee
     calleeip=$(eval ${getip})
     echo "callee IP: " ${calleeip}
-    scp -C juggler-callee root@${calleeip}:~
+    ssh-keygen -R ${calleeip}
+    scp -C -oStrictHostKeyChecking=no juggler-callee root@${calleeip}:~
+    ssh -n -f root@${calleeip} "sh -c 'nohup ~/juggler-callee -redis=${redisip}:7000'"
 
     # copy the load tool to juggler-load
     dropletname=juggler-load
     loadip=$(eval ${getip})
     echo "load IP: " ${loadip}
-    scp -C juggler-load root@${loadip}:~
+    ssh-keygen -R ${loadip}
+    scp -C -oStrictHostKeyChecking=no juggler-load root@${loadip}:~
 
     exit 0
 fi
