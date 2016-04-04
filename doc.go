288653bb2f0ef6978e283aa6945c0c244a262087
Martin Angers <martin.n.angers@gmail.com>
2016-04-04 19:38:23 -0400

    juggler: package doc

--- doc.go
+++ doc.go
@@ -1,5 +1,14 @@
 // Package juggler implements a websocket-based, redis-backed RPC and
-// pub-sub server.
+// pub-sub server. RPC (remote procedure calls) requests are routed
+// to a URI, and the response is asynchronous and is only sent to
+// the calling client. Pub-sub events are also asynchronous but
+// are routed to every client subscribed to the channel on which the
+// event is published. Only active clients receive the event - this is
+// not to be confused with a reliable message queue.
+//
+// All messages sent by the client receive an acknowledge message
+// (ACK) when processed successfully or a negative acknowledge (NACK)
+// if the request was rejected.
 //
 // Server
 //
@@ -32,6 +41,11 @@
 // is automatically enabled over HTTPS. See https://golang.org/doc/go1.6#http2
 // for details on how to explicitly disable it.
 //
+// One or many callees must be registered to listen for RPC requests.
+// The callees are decoupled from the server, with redis acting as the
+// broker. The pub-sub part is handled natively by redis. See the
+// callee package for details.
+//
 // Conn
 //
 // The Conn struct represents a websocket connection to a juggler
@@ -59,6 +73,49 @@
 //
 // Handler
 //
+// By default, when Server.Handler is nil, each message sent or received
+// by the Server goes through the ProcessMsg function, which implements
+// the standard behaviour for each message type.
 //
+// A custom handler can be set to implement middleware-style behaviour,
+// similar to the stdlib's net/http server. When Handler is not nil, it is
+// the responsibility of the handler to eventually call ProcessMsg so
+// that the messages produce the expected results.
+//
+// Typical use of handlers can be:
+//
+//     - to implement logging of requests/responses
+//     - to recover in case of panics
+//     - to implement authentication for some requests
+//     - to implement authorization checks for some requests
+//     - etc.
+//
+// If a handler detects that the message cannot be executed as requested,
+// e.g. because the caller is not authenticated or doesn't have access
+// to the requested RPC URI, ProcessMsg must not be called. The message
+// must be "intercepted" before the call to ProcessMsg, and a NACK reply
+// must be returned to the client to let it know that the request will
+// not be processed.
+//
+// To do so, the Conn offers the Send method, for example:
+//
+//     func CheckAccessHandler(ctx context.Context, c *juggler.Conn, m message.Msg) {
+//       // assume we detected that the caller doesn't have access
+//       if !ok {
+//         nack := message.NewNack(m, 403, errors.New("caller doesn't have access"))
+//         c.Send(nack)
+//         return
+//       }
+//     }
+//
+// The Send method makes sure the provided message goes through the handler
+// too, so typically a handler would implement conditional checks depending
+// on the type of the message. Authentication and authorization, for example,
+// only make sense on request messages (messages coming from the client),
+// which have their Type.IsRead method return true. Responses (messages
+// sent by the server) have their Type.IsWrite method return true.
+//
+// A new context.Context is passed for each message processed to maintain
+// values for the duration of a specific message.
 //
 package juggler
